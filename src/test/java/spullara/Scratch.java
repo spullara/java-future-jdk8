055f367758829143868ecdd0d00f0a496a72797f
Sam Pullara <spullara@yahoo.com>
2013-02-19 14:37:52 -0800

    fixed

--- src/test/java/spullara/Scratch.java
+++ src/test/java/spullara/Scratch.java
@@ -1,5 +1,7 @@
 package spullara;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
@@ -9,18 +11,17 @@ import java.util.function.Function;
 public class Scratch {
 
     static String address(InetSocketAddress sa) {
-        return elvis(elvis(sa, isa -> isa.getAddress()), InetAddress::getHostAddress);
+        return elvis(elvis(sa, InetSocketAddress::getAddress), InetAddress::getHostAddress);
     }
 
     public static <T, V> V elvis(T t, Function<T, V> mapper) {
         return t == null ? null : mapper.apply(t);
     }
 
-
     public static void test() {
         Map<String, Object> map = new HashMap<>();
         map.put("test", 1);
 
     }
-}
 
+}
